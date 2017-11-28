54e7bd7e050521fdaac079665e2a2f025be87bd2
BlaineMalone <bmalone05@qub.ac.uk>
2017-11-28 12:59:35 +0000

    Making http requests successfully. Reading robots.txt - now need to parse content.

--- src/Crawler.go
+++ src/Crawler.go
@@ -1,6 +1,53 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"log"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
-	
+func main() {
+	/*
+		Prepare url for http request
+	 */
+	var url string
+	if len(os.Args) != 2 {
+		url = "https://www.monzo.com/"
+	} else {
+		url = os.Args[1]
+	}
+	robotsFileUrl := prepareRobotsUrl(url)
+	fmt.Println("URL to Crawl: ", url);
+
+
+	/*
+		Make http GET request
+ 	*/
+	resp, err := http.Get(robotsFileUrl)
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(string(body))
+
+	/*
+		Make http GET request
+ 	*/
+	log.Printf("Fetching %v\n", url)
+	resp, err = http.Get(url)
+
+	if err != nil {
+		log.Fatal("Error GET request.")
+	}
+	defer resp.Body.Close() // Close body when read to completion - declared here fro readability
+	body, err = ioutil.ReadAll(resp.Body)
+	//fmt.Println(string(body))
+	fmt.Println("Hello Blaine")
+}
+
+func prepareRobotsUrl(url string) string {
+	if url[len(url)-1] != '/' {
+		url = url + "/"
+	}
+	return  url + "robots.txt"
 }
