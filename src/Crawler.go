87dc6e1f7b6988ac26e7f177b392bbf87d64e994
BlaineMalone <bmalone05@qub.ac.uk>
2017-11-29 14:33:49 +0000

    Updated crawler. TODO - Write tests

--- src/Crawler.go
+++ src/Crawler.go
@@ -3,90 +3,162 @@ package main
 import (
 	"net/http"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"golang.org/x/net/html"
 	"strings"
+	"net/url"
+	"io"
+	"sync"
+	"time"
 )
 
-
+var baseUrl string
+var visited sync.Map
+var mutex sync.RWMutex //Using a mutex to prevent interleaving of fmt.Println() betweek different gorountines.
 
 func main() {
 	/*
 		Prepare url for http request
 	 */
-	var url string
 	if len(os.Args) != 2 {
-		url = "https://www.monzo.com/"
+		baseUrl = "https://www.monzo.com/"
 	} else {
-		url = os.Args[1]
-	}
-	robotsFileUrl := prepareRobotsUrl(url)
-	fmt.Println("URL to Crawl: ", url);
-
-
-	/*
-		Make http GET request
- 	*/
-	resp, err := http.Get(robotsFileUrl)
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	fmt.Println(string(body) + "\n")
-
-	/*
-		Make http GET request
- 	*/
-	resp, err = http.Get(url)
-
-	if err != nil {
-		fmt.Println("ERROR: failed to GET data from ", url)
+		baseUrl = os.Args[1]
 	}
 
-	defer resp.Body.Close() // Close body when read to completion - declared here for readability
+	queue := make(chan string)
+	finished := make(chan bool)
 
-	/*
-		HTML Tokenizer - Parse html to extract tags
-		ErrorToken	Error during tokenization (or end of document)
-		StartTagToken	E.g. <a>
-	 */
-	tokenizer := html.NewTokenizer(resp.Body)
+	fmt.Printf("Generating site map for %s ...", baseUrl)
+	fmt.Println("")
+	go processAll(queue, finished)
+	queue <- baseUrl
+	<-finished
+	fmt.Println("\nComplete!")
+}
+
+/*
+	Continuously loops through urls that have been added to channel 'queue'.
+	If there is a long delay then break the loop and finish
+ */
+func processAll(queue chan string, finished chan bool) {
 	for {
-		nextToken := tokenizer.Next()
-		switch nextToken {
-		case html.ErrorToken: // End of the document, we're done
-			return
-		case html.StartTagToken:
-			currentToken := tokenizer.Token()
-			isAnchor := currentToken.Data == "a"
-			if !isAnchor { //Fail fast
+		select {
+		case url := <-queue:
+			if _, ok := visited.Load(url); ok {
 				continue
+			} else {
+				visited.Store(url, true)
+				go exploreUrl(url, queue)
 			}
-			ok, url := getHrefAttribute(currentToken) //Returns ok as 'true' if attribute found
-			if !ok {
-				continue
-			}
-			isExternalHttpLink := strings.Index(url, "http") == 0
-			if isExternalHttpLink {
-				fmt.Println(url)
+		case <-time.After(3 * time.Second):
+			finished <- true
+		}
+	}
+}
+/*
+	Scans current url and prints all links on the associated page. Does not follow external links.
+	Every new 'monzo' url that is discovered is added to channel queue for processing.
+ */
+func exploreUrl(url string, queue chan string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	mutex.Lock()
+	fmt.Println("\nFetching ", url)
+	defer resp.Body.Close()
+	linksOnPage := getLinksOnPage(resp.Body)
+	for _, link := range linksOnPage {
+		if isExternalLink(link) {
+			fmt.Println("\t" + link)
+		} else {
+			if url != "" {
+				absolute := formatUrl(link, url)
+				fmt.Println("\t" + absolute)
+				if _, ok := visited.Load(absolute); !ok {
+					queue <- absolute
+				}
 			}
 		}
 	}
-
+	mutex.Unlock()
 }
 
-func getHrefAttribute(t html.Token) (ok bool, href string) {
-	for _, a := range t.Attr {
-		if a.Key == "href" {
-			href = a.Val
-			ok = true
+/*
+	HTML Tokenizer - Parse html to extract tags
+	ErrorToken	Error during tokenization (or end of document)
+	StartTagToken	E.g. <a>
+ */
+func getLinksOnPage(body io.Reader) []string {
+	allLinks := []string{}
+	tokenizer := html.NewTokenizer(body)
+	for {
+		typeOfToken := tokenizer.Next()
+		if typeOfToken == html.ErrorToken {
+			return allLinks
+		}
+		token := tokenizer.Token()
+		if typeOfToken == html.StartTagToken && token.DataAtom.String() == "a" {
+			for _, attr := range token.Attr {
+				if attr.Key == "href" {
+					newLink := []string{ attr.Val }
+					addOnce(&allLinks, newLink) //pass address of allLinks - able to modify orig var
+				}
+			}
 		}
 	}
-	return
 }
 
-func prepareRobotsUrl(url string) string {
-	if url[len(url) - 1] != '/' {
-		url = url + "/"
+func isExternalLink(url string) bool {
+	isExternalLink :=
+		strings.Index(url, "http") == 0 ||
+			strings.Index(url, "/-play-store") == 0 || // redirects to external link
+			strings.Index(url, "/docs") == 0 || // robots.txt restricts scan on /docs
+			strings.Index(url, "tel:") == 0 ||
+			strings.Contains(url, "#")
+	return isExternalLink
+}
+
+
+/*
+	Formats href to absolute path e.g. /about --> https://www.monzo.com/about
+ */
+func formatUrl(href, base string) string {
+	uri, err := url.Parse(href)
+	if err != nil {
+		return ""
 	}
-	return url + "robots.txt"
+	baseUrl, err := url.Parse(base)
+	if err != nil {
+		return ""
+	}
+	uri = baseUrl.ResolveReference(uri)
+	return uri.String()
+}
+
+/*
+	Adding new found link to list of all links if it doesn't already contain new link.
+	Using reference to all links obj
+ */
+func addOnce(allLinks *[]string,  newLink []string) {
+	for _, str := range newLink {
+		if !contains(*allLinks, str) {
+			*allLinks = append(*allLinks, str)
+		}
+	}
+}
+
+/*
+	Checking to see new found link is not already contained in allLinks
+ */
+func contains(allLinks []string, newLink string) bool {
+	var check bool
+	for _, str := range allLinks {
+		if str == newLink {
+			check = true
+			break
+		}
+	}
+	return check
 }
